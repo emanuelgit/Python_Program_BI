8772bb66d5e6eeb569eff47e7b2a13ab4b34139f
emanuelgit <49260294+emanuelgit@users.noreply.github.com>
2019-11-27 18:20:43 -0300

    Add files via upload

---
+++ Clases/Clase_4/tarea 4.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+# Tarea 4
+# La tarea consiste en utilizar el archivo: "ventas.xlsx" y utilizando pandas 
+# agregar la columna "utilidad" que es: utilidad = Ventas - Costos.
+# y exportar el resultado a un excel (nuevo) y enviarlo por mail desde python.
+
+
