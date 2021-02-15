82f48cffa74add1d99d462638d422e2db60ebace
emanuelgit <49260294+emanuelgit@users.noreply.github.com>
2021-02-15 18:33:58 -0300

    Add files via upload

---
+++ Ejercicios.sql
@@ -0,0 +1,41 @@
+  /************ EJERCICIOS VARIOS DE PRACTICA *************/
+  
+  -- Select's 
+  -- 1) Hacer un select de la tabla Monedas donde la moneda sea igual a $$
+  
+  -- 2) Hacer un select de la tabla Salarios donde el Nombre empiece con D
+  
+  -- 3) Hacer un select de la tabla Salarios donde se muestre el Seccional (agrupado) y el total de Salario que tiene cada Seccional
+  
+  -- 4) Hacer un select de la tabla Monedas donde la fecha sea mayor o igual a 20-08-2019
+  
+  -- 5) Hacer un select de la tabla Salarios donde se muestre la facultad, el Seccional (agrupados) y la suma, promedio, maximo y minimo Salario
+  
+  -- 6) Hacer un select de tabla Salarios donde el ID_empleado termine en numero par.
+  
+  /* Subquery's */
+  -- 7) De la tabla Ventas obtener el promedio de utilidades
+  
+  -- 8) De la tabla Ventas obtener la suma por mes-año de las utilidades
+  
+  /* Funciones */
+  
+  -- 9) Basados en la tabla Ventas, crear una función llamada utilidad_acumulada (que reciba como parámetro dos fechas) que entregue la suma de utilidad entre un rango de fechas dado, 
+	si no estan las fechas entregar NULL.
+  
+  -- 10) Basados en la tabla Monedas, crear una funcion en donde dada una moneda distinta de 'CLP' y dada una fecha entregue el retorno en CLP de la moneda
+  
+  /* Vistas */
+  
+  -- 11) Basados en la tabla Monedas, crear una vista donde solo se vean las paridades para pasar a 'CLP'
+  
+  -- 12) Basados en la tabla Monedas, crear una vista donde solo se vean las paridades para pasar a 'USD'
+  
+  -- 13) Basados en la tabla Salarios crear una vista donde solo se vean solamente todo lo relacionado al Seccional 'Medellín'
+  
+  
+  
+
+  
+  
+  
